cf69faaaf0075ae73fd467ed3e3691ab6508abb5
HeroGamer153 <jshalin21@gmail.com>
2024-08-06 16:20:50 -0500

    tkinter

--- FantasyHelper.py
+++ FantasyHelper.py
@@ -1,67 +1,75 @@
 import gspread
 from sleeperpy import Leagues,Players,Drafts,User
 import time
+from tkinter import *
 
+root = Tk()
 
-account = User.get_user('sjoshi2004') #Username
+root.title("Fantasy Football App")
+root.configure(background="navy")
+root.geometry('350x200')
+root.mainloop()
 
-sa = gspread.service_account(filename="service_account.json")
-sheet = sa.open("Fantasy Rankings")
+def draftScript():
+    account = User.get_user('sjoshi2004') #Username
 
-wks1 = sheet.worksheet("Fantasy Pros PPR")
-wks2 = sheet.worksheet("LOEBS Leads PPR")
-wks3 = sheet.worksheet("NO BS PPR")
+    sa = gspread.service_account(filename="service_account.json")
+    sheet = sa.open("Fantasy Rankings")
 
-pick = 0
-userPick = 3
+    wks1 = sheet.worksheet("Fantasy Pros PPR")
+    wks2 = sheet.worksheet("LOEBS Leads PPR")
+    wks3 = sheet.worksheet("NO BS PPR")
 
-while pick < 150:
-    draft = Drafts.get_all_picks_in_draft(997031959723298816) #replace with draft ID
-    
-    if pick < len(draft):
-        pick_info = draft[pick]
-        pick_no = pick_info['pick_no']
+    pick = 0
+    userPick = 3
+
+    while pick < 150:
+        draft = Drafts.get_all_picks_in_draft(997031959723298816) #replace with draft ID
         
-        if pick == pick_no - 1:
-            firstName = pick_info['metadata']['first_name']
-            lastName = pick_info['metadata']['last_name']
-            Name = firstName + " " + lastName
-
-            cell1 = wks1.find(Name)
-
-            if cell1 is None:
-                print("Player Not Found: ", Name)
-            else:
-                row = str(cell1.row)
-                col = str(cell1.col)
-                location = "A" + row + ":" + "F" + row
-                wks1.format(location, {"backgroundColor": {"red": 25}})
-                print("Player drafted:", Name)
-
-            cell2 = wks2.find(Name)
-
-            if cell2 is None:
-                print("Player Not Found: ", Name)
-            else:
-                row2 = str(cell2.row)
-                col2 = str(cell2.col)
-                location2 = "A" + row2 + ":" + "E" + row2
-                wks2.format(location2, {"backgroundColor": {"red": 25}})
-                #print("Player drafted:", Name)
-
-            cell3 = wks3.find(Name)
-
-            if cell3 is None:
-                print("Player Not Found: ", Name)
-            else:
-                row3 = str(cell3.row)
-                col3 = str(cell3.col)
-                location3 = "A" + row3 + ":" + "F" + row3
-                wks3.format(location3, {"backgroundColor": {"red": 25}})
-                #print("Player drafted:", Name)
+        if pick < len(draft):
+            pick_info = draft[pick]
+            pick_no = pick_info['pick_no']
             
-            pick += 1
+            if pick == pick_no - 1:
+                firstName = pick_info['metadata']['first_name']
+                lastName = pick_info['metadata']['last_name']
+                Name = firstName + " " + lastName
 
-    # Wait for a period of time before checking again (e.g., every 10 seconds)
-    time.sleep(0.5)
+                cell1 = wks1.find(Name)
+
+                if cell1 is None:
+                    print("Player Not Found: ", Name)
+                else:
+                    row = str(cell1.row)
+                    col = str(cell1.col)
+                    location = "A" + row + ":" + "F" + row
+                    wks1.format(location, {"backgroundColor": {"red": 25}})
+                    print("Player drafted:", Name)
+
+                cell2 = wks2.find(Name)
+
+                if cell2 is None:
+                    print("Player Not Found: ", Name)
+                else:
+                    row2 = str(cell2.row)
+                    col2 = str(cell2.col)
+                    location2 = "A" + row2 + ":" + "E" + row2
+                    wks2.format(location2, {"backgroundColor": {"red": 25}})
+                    #print("Player drafted:", Name)
+
+                cell3 = wks3.find(Name)
+
+                if cell3 is None:
+                    print("Player Not Found: ", Name)
+                else:
+                    row3 = str(cell3.row)
+                    col3 = str(cell3.col)
+                    location3 = "A" + row3 + ":" + "F" + row3
+                    wks3.format(location3, {"backgroundColor": {"red": 25}})
+                    #print("Player drafted:", Name)
+                
+                pick += 1
+
+        # Wait for a period of time before checking again (e.g., every 10 seconds)
+        time.sleep(0.5)
 
